dbf4e51a43cb86344fd4c3319d2b8b1f2e901340
ryuuzaki <jackzxty@126.com>
2013-06-14 10:25:09 +0800

    搭建hixo框架

---
+++ include/common.h
@@ -0,0 +1,13 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __COMMON_H__
---
+++ src/hixo.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
