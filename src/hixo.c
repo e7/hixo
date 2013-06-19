6efb8cd132c783150df9def561a3d913af28d472
ryuuzaki <jackzxty@126.com>
2013-06-19 19:11:16 +0800

    整理监听套接字部分代码

--- src/hixo.c
+++ src/hixo.c
@@ -17,6 +17,7 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <sys/resource.h>
 #include <sys/socket.h>
 #include <sys/epoll.h>
 #include <arpa/inet.h>
@@ -33,6 +34,7 @@
 typedef struct {
     uint32_t m_ip;
     uint16_t m_port;
+    int m_backlog;
 } addr_t;
 
 
@@ -41,20 +43,97 @@ typedef struct {
 #define WORKER_PROCESSES        4
 #define MAX_CONNECTIONS         64
 #define CONNECTION_TIME_OUT     60
-static addr_t s_srv_addrs[] = {
-    {INADDR_ANY, 80},
-    {INADDR_ANY, 8888},
-    {INADDR_ANY, 8889},
-    {INADDR_ANY, 8890},
+addr_t const SRV_ADDRS[] = {
+    {INADDR_ANY, 80,   0},
+    {INADDR_ANY, 8888, 0},
+    {INADDR_ANY, 8889, 0},
+    {INADDR_ANY, 8890, 0},
 };
+#define SRV_ADDRS_COUNT         ARRAY_COUNT(SRV_ADDRS)
 // }} config
 
+// sysconf {{
+struct {
+    int const MAX_FILE_NO;
+} g_sysconf;
+// }} sysconf
 
+
+#ifndef ESUCCESS
+    #define ESUCCESS        (0)
+#endif
 #define HIXO_OK             (0)
 #define HIXO_ERROR          (-1)
 #define unblocking_fd(fd)   fcntl(fd, \
                                   F_SETFL, \
                                   fcntl(fd, F_GETFL) | O_NONBLOCK)
+
+// bitmap {{
+typedef struct {
+    byte_t *mp_data;
+    uint32_t m_size;
+} bitmap_t;
+
+int create_bitmap(bitmap_t *p_bm, uint32_t nbits)
+{
+    if (0 == nbits) {
+        return HIXO_ERROR;
+    }
+
+    p_bm->m_size = nbits / sizeof(byte_t) + 1;
+    p_bm->mp_data = calloc(1, p_bm->m_size);
+    if (NULL == p_bm->mp_data) {
+        return HIXO_ERROR;
+    }
+    (void)memset(p_bm->mp_data, 0, p_bm->m_size);
+}
+
+int bitmap_set(bitmap_t *p_bm, uint32_t bit_offset)
+{
+    uint32_t byte_offset = bit_offset / sizeof(byte_t);
+    uint32_t byte_bit_offset = bit_offset % sizeof(byte_t);
+
+    if (bit_offset > (sizeof(byte_t) * p_bm->m_size - 1)) {
+        fprintf(stderr, "[ERROR] bitmap bit_offset out of range\n");
+
+        return HIXO_ERROR;
+    }
+    
+    p_bm->mp_data[byte_offset] &= 1 << (sizeof(byte_t) - byte_bit_offset);
+
+    return HIXO_OK;
+}
+
+int bitmap_is_set(bitmap_t *p_bm, uint32_t bit_offset)
+{
+    uint32_t byte_offset = bit_offset / sizeof(byte_t);
+    uint32_t byte_bit_offset = bit_offset % sizeof(byte_t);
+
+    if (bit_offset > (sizeof(byte_t) * p_bm->m_size - 1)) {
+        fprintf(stderr, "[ERROR] bitmap bit_offset out of range\n");
+
+        return HIXO_ERROR;
+    }
+
+    if (p_bm->mp_data[byte_offset]
+            & (1 << (sizeof(byte_t) - byte_bit_offset)))
+    {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+
+}
+
+void destroy_bitmap(bitmap_t *p_bm)
+{
+    if (NULL != p_bm->mp_data) {
+        free(p_bm->mp_data);
+        p_bm->mp_data = NULL;
+    }
+    p_bm->m_size = 0;
+}
+// }} bitmap
 
 
 typedef enum {
@@ -73,6 +152,128 @@ struct s_hixo_connection_t {
     int m_shut_write;
     list_t m_shut_write_node;
 };
+
+
+// hixo_listen_t {{
+typedef enum {
+    CLOSED = 0,
+    OPENED = CLOSED + 1,
+    CONFIGURED = OPENED + 1,
+    BOUND = CONFIGURED + 1,
+    LISTENING = BOUND + 1,
+} listening_status_t;
+
+typedef struct s_hixo_listening_t hixo_listening_t;
+struct s_hixo_listening_t {
+    int m_fd;
+    listening_status_t m_status;
+    list_t *mp_conn_list;
+};
+
+hixo_listening_t ga_hixo_listenings[SRV_ADDRS_COUNT] = {
+    {0, CLOSED, NULL},
+};
+
+int hixo_init_listenings(void)
+{
+    int tmp_err = 0;
+    int arrived_count = 0;
+    struct sockaddr_in srv_addr;
+
+    for (int i = 0; i < SRV_ADDRS_COUNT; ++i) {
+        int sock_fd = 0;
+
+        errno = 0;
+        sock_fd = socket(PF_INET, SOCK_STREAM, 0);
+        tmp_err = (-1 == sock_fd) ? errno : 0;
+        if (ESUCCESS == tmp_err) {
+            ga_hixo_listenings[i].m_fd = sock_fd;
+            ga_hixo_listenings[i].m_status = OPENED;
+        } else {
+            ga_hixo_listenings[i].m_status = CLOSED;
+        }
+    }
+
+    for (int i = 0; i < SRV_ADDRS_COUNT; ++i) {
+        int ret = 0;
+
+        if (OPENED != ga_hixo_listenings[i].m_status) {
+            continue;
+        }
+        
+        errno = 0;
+        ret = unblocking_fd(ga_hixo_listenings[i].m_fd);
+        tmp_err = (-1 == ret) ? errno : 0;
+        if (ESUCCESS == tmp_err) {
+            ga_hixo_listenings[i].m_status = CONFIGURED;
+        } else {
+            (void)close(ga_hixo_listenings[i].m_fd);
+            ga_hixo_listenings[i].m_status = CLOSED;
+        }
+    }
+
+    (void)memset(&srv_addr, 0, sizeof(srv_addr));
+    for (int i = 0; i < SRV_ADDRS_COUNT; ++i) {
+        int ret = 0;
+
+        if (CONFIGURED != ga_hixo_listenings[i].m_status) {
+            continue;
+        }
+
+        srv_addr.sin_family = AF_INET;
+        srv_addr.sin_addr.s_addr = htonl(SRV_ADDRS[i].m_ip);
+        srv_addr.sin_port = htons(SRV_ADDRS[i].m_port);
+
+        errno = 0;
+        ret = bind(ga_hixo_listenings[i].m_fd,
+                   (struct sockaddr *)&srv_addr,
+                   sizeof(srv_addr));
+        tmp_err = (-1 == ret) ? errno : 0;
+        if (ESUCCESS == tmp_err) {
+            ga_hixo_listenings[i].m_status = BOUND;
+        } else {
+            (void)close(ga_hixo_listenings[i].m_fd);
+            ga_hixo_listenings[i].m_status = CLOSED;
+        }
+    }
+
+    arrived_count = 0;
+    for (int i = 0; i < SRV_ADDRS_COUNT; ++i) {
+        int ret = 0;
+
+        if (BOUND != ga_hixo_listenings[i].m_status) {
+            continue;
+        }
+
+        errno = 0;
+        ret = listen(ga_hixo_listenings[i].m_fd,
+                     (SRV_ADDRS[i].m_backlog > 0)
+                         ? SRV_ADDRS[i].m_backlog
+                         : SOMAXCONN);
+        tmp_err = (-1 == ret) ? errno : 0;
+        if (ESUCCESS == tmp_err) {
+            ga_hixo_listenings[i].m_status = LISTENING;
+            ++arrived_count;
+        } else {
+            (void)close(ga_hixo_listenings[i].m_fd);
+            ga_hixo_listenings[i].m_status = CLOSED;
+        }
+    }
+
+    return arrived_count ? HIXO_OK : HIXO_ERROR;
+}
+
+void hixo_uninit_listenings(void)
+{
+    for (int i = 0; i < SRV_ADDRS_COUNT; ++i) {
+        if (ga_hixo_listenings[i].m_status > CLOSED) {
+            (void)close(ga_hixo_listenings[i].m_fd);
+            ga_hixo_listenings[i].m_status = CLOSED;
+        }
+    }
+}
+// }} hixo_listen_t
+
 
 typedef struct {
     int (*mpf_init)(void);
@@ -183,7 +384,7 @@ int epoll_process_events(void)
     }
 
     for (int i = 0; i < nevents; ++i) {
-        hixo_connection_t *p_conn = p_evs[i].data.ptr;
+        hixo_connection_t *p_conn = (hixo_connection_t *)p_evs[i].data.ptr;
 
         if (p_evs[i].events & (EPOLLERR | EPOLLHUP)) {
             p_conn->m_shut_read = TRUE;
@@ -230,11 +431,12 @@ hixo_module_t *g_modules[] = {
 };
 
 
-int g_master = TRUE;
 static struct {
     int m_fd;
     int m_valid;
-} s_lsn_sockets[ARRAY_COUNT(s_srv_addrs)];
+} s_lsn_sockets[ARRAY_COUNT(SRV_ADDRS)] = {};
+bitmap_t g_lsn_sockets_bm = {NULL, 0};
+int g_master = TRUE;
 
 static int master_main(void)
 {
@@ -273,9 +475,25 @@ static int worker_main(void)
 
 static int hixo_main(void)
 {
+    int rslt = 0;
+    struct rlimit rlmt;
+    int tmp_err = 0;
+    int *p_file_no = (int *)&g_sysconf.MAX_FILE_NO;
+
+    errno = 0;
+    rslt = getrlimit(RLIMIT_NOFILE, &rlmt);
+    tmp_err = (-1 == rslt) ? errno : 0;
+    if (tmp_err) {
+        fprintf(stderr, "[ERROR] getrlimit() failed: %d\n", tmp_err);
+
+        return HIXO_ERROR;
+    }
+    *p_file_no = rlmt.rlim_cur;
+
     if (DAEMON) {
     }
 
+    // 分裂进程
     for (int i = 0; i < WORKER_PROCESSES; ++i) {
         pid_t cpid = fork();
         if (-1 == cpid) {
@@ -290,135 +508,27 @@ static int hixo_main(void)
     }
 
     if (g_master) {
-        return master_main();
+        rslt = master_main();
     } else {
-        return worker_main();
+        rslt = worker_main();
     }
+
+    return rslt;
 }
 
 int main(int argc, char *argv[])
 {
     int rslt = EXIT_FAILURE;
-    int tmp_err = 0;
-    struct sockaddr_in srv_addr;
-    int fatal_err = 0;
 
-    for (int i = 0; i < ARRAY_COUNT(s_srv_addrs); ++i) {
-        s_lsn_sockets[i].m_fd = -1;
-        s_lsn_sockets[i].m_valid = TRUE;
+    if (HIXO_ERROR == hixo_init_listenings()) {
+        return EXIT_FAILURE;
     }
 
-    // 创建套接字
-    fatal_err = TRUE;
-    for (int i = 0; i < ARRAY_COUNT(s_srv_addrs); ++i) {
-        errno = 0;
-        s_lsn_sockets[i].m_fd = socket(PF_INET, SOCK_STREAM, 0);
-        tmp_err = (-1 == s_lsn_sockets[i].m_fd) ? errno : 0;
-        if (tmp_err) {
-            s_lsn_sockets[i].m_valid = FALSE;
-            fprintf(stderr, "[ERROR] socket() failed: %d\n", tmp_err);
-        } else {
-            fatal_err = FALSE;
-        }
+    rslt = (HIXO_ERROR == hixo_main()) ? EXIT_SUCCESS : EXIT_FAILURE;
+
+    if (g_master) {
+        hixo_uninit_listenings();
     }
-
-    if (fatal_err) {
-        goto ERR_SOCKET;
-    }
-
-    // 非阻塞
-    fatal_err = TRUE;
-    for (int i = 0; i < ARRAY_COUNT(s_srv_addrs); ++i) {
-        int ret = 0;
-
-        if (!s_lsn_sockets[i].m_valid) {
-            continue;
-        }
-
-        errno = 0;
-        ret = unblocking_fd(s_lsn_sockets[i].m_fd);
-        tmp_err = (-1 == ret) ? errno : 0;
-        if (tmp_err) {
-            s_lsn_sockets[i].m_valid = FALSE;
-            fprintf(stderr, "[ERROR] fcntl() failed: %d\n", tmp_err);
-        } else {
-            fatal_err = FALSE;
-        }
-    }
-
-    if (fatal_err) {
-        goto ERR_FCNTL;
-    }
-
-    // 绑定
-    fatal_err = TRUE;
-    (void)memset(&srv_addr, 0, sizeof(srv_addr));
-    for (int i = 0; i < ARRAY_COUNT(s_srv_addrs); ++i) {
-        int ret = 0;
-
-        if (!s_lsn_sockets[i].m_valid) {
-            continue;
-        }
-
-        srv_addr.sin_family = AF_INET;
-        srv_addr.sin_addr.s_addr = htonl(s_srv_addrs[i].m_ip);
-        srv_addr.sin_port = htons(s_srv_addrs[i].m_port);
-        errno = 0;
-        ret = bind(s_lsn_sockets[i].m_fd,
-                   (struct sockaddr *)&srv_addr,
-                   sizeof(srv_addr));
-        tmp_err = (-1 == ret) ? errno : 0;
-        if (tmp_err) {
-            s_lsn_sockets[i].m_valid = FALSE;
-            fprintf(stderr, "[ERROR] bind() failed: %d\n", tmp_err);
-        } else {
-            fatal_err = FALSE;
-        }
-    }
-
-    if (fatal_err) {
-        goto ERR_BIND;
-    }
-
-    // 监听
-    fatal_err = TRUE;
-    for (int i = 0; i < ARRAY_COUNT(s_srv_addrs); ++i) {
-        int ret = 0;
-
-        if (!s_lsn_sockets[i].m_valid) {
-            continue;
-        }
-
-        errno = 0;
-        ret = listen(s_lsn_sockets[i].m_fd, SOMAXCONN);
-        tmp_err = (-1 == ret) ? errno : 0;
-        if (tmp_err) {
-            s_lsn_sockets[i].m_valid = FALSE;
-            fprintf(stderr, "[ERROR] listen failed: %d\n", tmp_err);
-        } else {
-            fatal_err = FALSE;
-        }
-    }
-
-    if (fatal_err) {
-        goto ERR_LISTEN;
-    }
-
-    rslt = (0 == hixo_main()) ? EXIT_SUCCESS : EXIT_FAILURE;
-
-    // 错误处理
-ERR_LISTEN:
-
-ERR_BIND:
-
-ERR_FCNTL:
-    for (int i = 0; i < ARRAY_COUNT(s_srv_addrs); ++i) {
-        if (s_lsn_sockets[i].m_valid) {
-            (void)close(s_lsn_sockets[i].m_fd);
-        }
-    }
-
-ERR_SOCKET:
 
     return rslt;
 }
